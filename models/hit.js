525e493a2d741f35b3d59e890d32c596c273c030
LuosRestil <metaltasteslikechicken@gmail.com>
2020-05-25 14:29:06 -0500

    fixed schema error

--- models/hit.js
+++ models/hit.js
@@ -5,6 +5,6 @@ const HitSchema = new Schema({
   hits: { type: Number, required: true },
 });
 
-const Hit = mongoose.model("Hit", IssueSchema);
+const Hit = mongoose.model("Hit", HitSchema);
 
 module.exports = Issue;
